75b97e36285d94002587ed9ac43ead88b63f4e3d
murilo <murilo1409.97@gmail.com>
2019-02-26 19:45:36 -0300

    testes

---
+++ teste.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+# get audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Diga algo :")
+    audio = r.listen(source)
+
+try:
+    print("Você disse : " + r.recognize_google(audio, language='pt-BR'))
+except sr.UnknownValueError:
+    print("Não consegui te entender ")
+except sr.RequestError as e:
+    print("vc esta offline; {0}".format(e))
